783487fe54451f2a9ae5cab524c461575642701b
Liudmila Nekrasova <l.nekrasova@splinestudio.ua>
2023-02-28 17:03:48 +0200

    create product card

---
+++ src/components/AppProductCard.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='productCard'>
+    <img src='' alt='' class='productCard__img'>
+    <p class='productCard__label'></p>
+    <div class='productCard__description'>
+      <h2 class='productCard__title'></h2>
+      <p class='productCard__price'></p>
+      <p class='productCard__seasons'></p>
+      <div class='productCard__sizes'></div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
